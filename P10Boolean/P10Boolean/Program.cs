b2f25ff0091f0c991540b7e9e095932fc916c359
StepounJames <stevan.stepanovic@student.forsbergsskola.se>
2024-09-27 01:23:23 +0200

    P10Boolean
    Boolin

---
+++ P10Boolean/P10Boolean/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("How old are you?");
+string ageS = Console.ReadLine();
+int age = Convert.ToInt32(ageS);
+bool isChild = 0 <= age && age <= 12;
+bool isTeenager = 13 <= age && age <= 19;
+bool isAdult = 19 < age;
+if (isChild)
+{
+    Console.WriteLine("You're a child");
+}
+else if (isTeenager)
+{
+    Console.WriteLine("You're a teenager");
+}
+else if (isAdult)
+{
+    Console.WriteLine("You're an adult");
+}
+else
+{
+    Console.WriteLine("Please try again");
+}
