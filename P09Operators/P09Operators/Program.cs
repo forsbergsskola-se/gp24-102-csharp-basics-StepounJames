13e4ef0df2452dba52ef46a51dd1db99d4873a6c
StepounJames <stevan.stepanovic@student.forsbergsskola.se>
2024-09-27 01:47:49 +0200

    P9Operators
    Piss break

---
+++ P09Operators/P09Operators/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Give me a number of seconds.");
+string number = Console.ReadLine();
+float daysb = int.Parse(number);
+int days = (int)daysb / 86400;
+
+
+
+
+
+Console.WriteLine(days + " days");
