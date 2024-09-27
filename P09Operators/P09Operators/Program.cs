1b7a8c2e56d5165dd7fb934a6aacad22e138f31f
StepounJames <stevan.stepanovic@student.forsbergsskola.se>
2024-09-27 02:13:29 +0200

    P09_00Operators
    00 done, commented

--- P09Operators/P09Operators/Program.cs
+++ P09Operators/P09Operators/Program.cs
@@ -1,10 +1,18 @@
-﻿Console.WriteLine("Give me a number of seconds.");
+﻿/*Console.WriteLine("Give me a number of seconds.");
 string number = Console.ReadLine();
 float daysb = int.Parse(number);
 int days = (int)daysb / 86400;
+float hoursb = int.Parse(number) - (86400 * days);
+int hours = (int)hoursb / 3600;
+float minutesb = int.Parse(number) - (86400 * days) - (3600 * hours);
+int minutes = (int)minutesb / 60;
+float seconds = (int)minutesb % 60;
+float decdaysb = int.Parse(number);
+float decdays = decdaysb / 86400;
+Console.WriteLine("Seconds: " + seconds);
+Console.WriteLine("Minutes: " + minutes);
+Console.WriteLine("Hours: " + hours);
+Console.WriteLine("Days: " + days);
+Console.WriteLine(days + "." + hours + ":" + minutes + ":" + seconds);
+Console.WriteLine("In total, that's:" + decdays + " days.");*/
 
-
-
-
-
-Console.WriteLine(days + " days");
